9348b26e258bbb0a2d74753467fa422efaff3d1f
student <student@ip-172-31-51-12.us-west-2.compute.internal>
2022-01-27 16:58:07 -0800

    finished my own adventure

--- src/_01_else_if/_5_own_adventure/MyOwnAdventure.java
+++ src/_01_else_if/_5_own_adventure/MyOwnAdventure.java
@@ -18,16 +18,16 @@ public class MyOwnAdventure {
 				if (diveNum == 0) {
 					JOptionPane.showMessageDialog(null, "You have gained immortality from this drink! You win the game!");
 				}
-				if (diveNum == 1) {
+				else if (diveNum == 1) {
 					JOptionPane.showMessageDialog(null, "You died from a lethal poison. Game Over.");
 				}
 			}
-			if (drink.equalsIgnoreCase("no")) {
+			else if (drink.equalsIgnoreCase("no")) {
 				JOptionPane.showMessageDialog(null, "You drown from staying underwater too long wondering what to do. Game Over.");
 			}
 		}
 //swim down
-		if (firstQuestion.equalsIgnoreCase("swim down")) {
+		else if (firstQuestion.equalsIgnoreCase("swim down")) {
 			String lake = JOptionPane.showInputDialog("You come across a lake at the bottom of the river. What do you do? Keep swimming or get out.");
 			if (lake.equalsIgnoreCase("keep swimming")) {
 				int lakeNum = ran.nextInt(2);
@@ -36,19 +36,39 @@ public class MyOwnAdventure {
 					if (dolphin.equalsIgnoreCase(yes)) {
 						JOptionPane.showMessageDialog(null, "The pill grants you immortality! You win the game!");
 					}
-					if (dolphin.equalsIgnoreCase(no)) {
+					else if (dolphin.equalsIgnoreCase(no)) {
 						JOptionPane.showMessageDialog(null, "The dolphin slaps you and you die. Game Over.");
 					}
 				}
-				if (lakeNum == 1) {
+				else if (lakeNum == 1) {
 					JOptionPane.showMessageDialog(null, "You die from dehydration after swimming for too long. Game Over.");
 				}
 			}
-			if (lake.equalsIgnoreCase("get out")) {
+			else if (lake.equalsIgnoreCase("get out")) {
 				JOptionPane.showMessageDialog(null, "You die from hypothermia after getting out to fast. Game Over.");
 			}
 		}
 //swim up
+		else if (firstQuestion.equalsIgnoreCase("swim up")) {
+			String village = JOptionPane.showInputDialog("You come across a village at the top of the river. What do you do? Go to the village or keep swimming?");
+			if (village.equalsIgnoreCase("go to the village")) {
+				int villageNum = ran.nextInt(2);
+				if (villageNum == 0) {
+					JOptionPane.showMessageDialog(null, "The village people attack you and kill you for contaminating their river water. Game Over.");
+				}
+				else if (villageNum == 1) {
+					JOptionPane.showMessageDialog(null, "The village people think you are a river god and give you a dumpling of immortality! You win the game!");
+					
+				}
+			}
+			else if (village.equalsIgnoreCase("keep swimming")) {
+				JOptionPane.showMessageDialog(null, "You die from dehydration after swimming for too long. Game Over.");
+			}
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You die from drowning. Game Over.");
+		}
+		
 	}
 	
 }
