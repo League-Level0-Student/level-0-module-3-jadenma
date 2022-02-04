a2709ee13d4c7e6ad90cea37bfe1b1f61880823f
student <student@ip-172-31-57-201.us-west-2.compute.internal>
2022-02-03 17:06:56 -0800

    Did years alive, shiny objects, and walk of fame

---
+++ src/_02_loop_variables/_1_years_alive/YearsAlive.java
@@ -0,0 +1,9 @@
+package _02_loop_variables._1_years_alive;
+
+public class YearsAlive {
+	public static void main(String[] args) {
+		for (int firstYear=2008; firstYear<=2022; firstYear++) {
+			System.out.println(firstYear);
+		}
+	}
+}
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,14 +6,30 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		
+		int stars = 10;
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setSpeed(100);
+		rob.setX(10);
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		while (stars > 0) {
+			int side = 5;
+			while (side > 0) {
+				rob.turn(144);
+				rob.move(30);
+				side--;
+			}
+			rob.setAngle(90);
+			rob.penUp();
+			rob.move(50);
+			rob.penDown();
+			rob.setAngle(0);
+			stars--;
+		}
+		rob.hide();
 		// 3. Set the length of each line in the star to 30.
-
+		
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
